218b718112ec0fdf18f52daf536ce9ada1f5c7af
timeiscoal <timeiscoal@gmail.com>
2023-02-02 17:14:48 +0900

    to do list

--- ReactFolder/my-app/src/App.js
+++ ReactFolder/my-app/src/App.js
@@ -1,37 +1,44 @@
-import { useState , useEffect} from "react";
+import { useState } from "react";
 
-// 컨포넌트가 사라지거나 없어 질때의 반응 또한 만들 수 있다. return () => 
-function Hello(){
-  function byFn(){
-    console.log("bye :(")
-  }
 
-  function effectFn(){
-    console.log("Create :)");
-    return byFn
-  }
-  useEffect(effectFn,[])
-  return <h1>Hello</h1>
-}
 
 function App() {
-  const [showing,setShowing] = useState(false);
-  const onClick=()=>{
-    setShowing((prev)=>!prev)
-  }
+
+  // todo
+  const [toDo, setToDo] = useState("")
+  // todo list
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {event.preventDefault(); 
+    if (toDo ===""){
+      return;
+    };
+    setToDo("");
+    setToDos((currentArray)=> [toDo , ...currentArray]);
+  };
 
   return (
     <div>
+      <h1>My To Do list : {toDos.length}</h1>
+      <form onSubmit={onSubmit}>
+        <input  onChange={onChange} value={toDo}  type="text" placeholder="Write your to do" />
+        
+        <button>Add To Do List</button>
 
-    <h1>Hello World</h1>
 
-    {showing ? <Hello /> : null}  
-    <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
-    
+      </form>
+      <hr/>
+      <ul>
+        {toDos.map((item, index) => <li key={index}>{item}</li>)}
+      </ul>
+
     </div>
 
     
   );
 }
+// toDos.map((item,index) => <li key={index}>{item}</li>)
+// toDos라는 array를 가져와 map의 첫번째 argument로 변형한 후에 retrun해준다.
+// 반대로 나오게 하고 싶으면 reverse()
 
 export default App;
